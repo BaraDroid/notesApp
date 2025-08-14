91b6b0a3a058924e3d65092dead83916609fb93d
BaraDroid <lambeinovab@seznam.cz>
2025-08-14 16:45:14 +0200

    bug: new notes don´t overwrite all local storage any more

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let savedNotes = [];
 const notesContainer = document.getElementById("savedNotesContentRef");
 
-window.addEventListener('load', renderSavedNotes);
+window.addEventListener("load", renderSavedNotes);
 
 function saveNote() {
-    getData();
-    let sortedNotes = savedNotes.reverse();
-    notesContainer.innerHTML = '';
-    sortedNotes.forEach((note) => {getNoteCardTemplate(note)});
-    savedNotes = sortedNotes.reverse();
-    clearInputField();
-    saveNotesInLS();
+  getData();
+  let sortedNotes = savedNotes.reverse();
+  notesContainer.innerHTML = "";
+  sortedNotes.forEach((note) => {
+    getNoteCardTemplate(note);
+  });
+  savedNotes = sortedNotes.reverse();
+  clearInputField();
+  saveNotesInLS();
 }
 
 function renderSavedNotes() {
-  notesContainer.innerHTML = '';
-  let currentSavedNotes = getNotesFromLS();
-  let sortedSavedNotes = currentSavedNotes.reverse();
-  sortedSavedNotes.forEach(note => getNoteCardTemplate(note));
+  if (getNotesFromLS() !== null) {
+    savedNotes = getNotesFromLS();
+    notesContainer.innerHTML = "";
+    let sortedSavedNotes = savedNotes.reverse();
+    sortedSavedNotes.forEach((note) => getNoteCardTemplate(note));
+    savedNotes.reverse();
+  }
 }
 
 function getData() {
@@ -34,40 +39,40 @@ function getData() {
 }
 
 function getNoteCardTemplate(oneNote) {
-    const noteCard = document.createElement("div");
-    noteCard.classList.add("note_card");
-    const title = document.createElement("span");
-    title.classList.add("note_title");
-    title.innerText = oneNote.noteTitle;
-    const noteContent = document.createElement("span");
-    noteContent.classList.add("note_content");
-    noteContent.innerText = oneNote.noteBody;
-    const noteUpdate = document.createElement("span");
-    noteUpdate.classList.add("created_at");
-    noteUpdate.innerText = oneNote.lastUpdated;
+  const noteCard = document.createElement("div");
+  noteCard.classList.add("note_card");
+  const title = document.createElement("span");
+  title.classList.add("note_title");
+  title.innerText = oneNote.noteTitle;
+  const noteContent = document.createElement("span");
+  noteContent.classList.add("note_content");
+  noteContent.innerText = oneNote.noteBody;
+  const noteUpdate = document.createElement("span");
+  noteUpdate.classList.add("created_at");
+  noteUpdate.innerText = oneNote.lastUpdated;
 
-    notesContainer.appendChild(noteCard);
-    noteCard.appendChild(title);
-    noteCard.appendChild(noteContent);
-    noteCard.appendChild(noteUpdate);
+  notesContainer.appendChild(noteCard);
+  noteCard.appendChild(title);
+  noteCard.appendChild(noteContent);
+  noteCard.appendChild(noteUpdate);
 }
 
-
-
-
-
 function clearInputField() {
-    document.getElementById("noteTitle").value = '';
-    document.getElementById("noteContent").value = '';
+  document.getElementById("noteTitle").value = "";
+  document.getElementById("noteContent").value = "";
 }
 
 function saveNotesInLS() {
-    localStorage.setItem('savedNotes', JSON.stringify(savedNotes));
+  let notesInLocalStorage = localStorage.setItem(
+    "savedNotes",
+    JSON.stringify(savedNotes)
+  );
+  return notesInLocalStorage;
 }
 
 function getNotesFromLS() {
-    let savedNotes = JSON.parse(localStorage.getItem('savedNotes'));
-    return savedNotes;
+  let savedNotes = JSON.parse(localStorage.getItem("savedNotes"));
+  return savedNotes;
 }
 
 function deleteNote() {
@@ -75,11 +80,10 @@ function deleteNote() {
 }
 
 function editNote(noteId) {
-    const savedNote = {
-        savedNoteTitle: '',
-        savedNoteContent: ''
-    }
-
+  const savedNote = {
+    savedNoteTitle: "",
+    savedNoteContent: "",
+  };
 }
 
 function saveEditedNote() {}

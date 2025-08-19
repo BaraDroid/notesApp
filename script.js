67414447d93d0b07cd3fcc14d29b206f92476b6b
BaraDroid <lambeinovab@seznam.cz>
2025-08-19 07:55:49 +0200

    all onclicks changed to event listeners

--- script.js
+++ script.js
@@ -2,9 +2,28 @@ let savedNotes = [];
 const notesContainer = document.getElementById("savedNotesContentRef");
 const titleInputField = document.getElementById("noteTitle");
 const contentInputField = document.getElementById("noteContent");
-const alertMessage = document.getElementById('alert');
+const alertMessage = document.getElementById("alert");
+
+const newNoteBtnEl = document.querySelector(".add_new");
+const saveNoteBtnEl = document.querySelector("#saveBtn");
+const deleteNoteBtnEl = document.querySelector("#deleteBtn");
+const dismissAlertBtnEl = document.querySelector(".dismiss_btn");
 
 window.addEventListener("load", renderSavedNotes);
+newNoteBtnEl.addEventListener("click", createNewNote);
+saveNoteBtnEl.addEventListener("click", saveNote);
+deleteNoteBtnEl.addEventListener("click", deleteNote);
+dismissAlertBtnEl.addEventListener("click", dismissAlert);
+
+function createNewNote() {
+  document
+    .querySelectorAll(".note_card")
+    .forEach((card) => card.classList.remove("note_selected"));
+  getData();
+  renderSavedNotes();
+  clearInputField();
+  saveNotesInLS();
+}
 
 function saveNote() {
   getData();
@@ -18,37 +37,10 @@ function saveNote() {
   saveNotesInLS();
 }
 
-
-function createNewNote() {
-  //nojo, ted se mi otevre nova, ale ta stara se ulozi jako bez obsahu
-  //tady fakt musim tu starou nejdriv ulozit! ok, to mam, ale to mi ji ulozi nahore, i kdyz tam neni zadna zmena
-  //to musim nejak osetrit, ze kdyz zadny zmeny, tak se nic nedeje
-    document.querySelectorAll('.note_card').forEach(card => card.classList.remove('note_selected'));
-  console.log('creating new note');
-  getData();
-  
-  renderSavedNotes();
-  clearInputField();
-  saveNotesInLS();
-}
-
-
-
-
-function renderSavedNotes() {
-  if (getNotesFromLS() !== null) {
-    savedNotes = getNotesFromLS();
-    notesContainer.innerHTML = "";
-    let sortedSavedNotes = savedNotes.reverse();
-    sortedSavedNotes.forEach((note) => getNoteCardTemplate(note));
-    savedNotes.reverse();
-  }
-}
-
 function getData() {
   const noteTitle = titleInputField.value;
   const noteContent = contentInputField.value;
-  if(noteTitle === '' || noteContent === '') {
+  if (noteTitle === "" || noteContent === "") {
     renderToastMessage();
     return;
   }
@@ -61,10 +53,51 @@ function getData() {
   savedNotes.push(myNote);
 }
 
+function editNote(noteToEdit, clickedCard) {
+  document
+    .querySelectorAll(".note_card")
+    .forEach((card) => card.classList.remove("note_selected"));
+  clickedCard.classList.add("note_selected");
+  if (
+    titleInputField.value === noteToEdit.noteTitle &&
+    contentInputField.value === noteToEdit.noteBody
+  ) {
+    noteToEdit.lastUpdated = noteToEdit.lastUpdate;
+  } else {
+    titleInputField.value = noteToEdit.noteTitle;
+    contentInputField.value = noteToEdit.noteBody;
+    noteToEdit.lastUpdated = new Date().toLocaleString();
+  }
+  console.log("noteToEdit ID", noteToEdit.id);
+  let newArray = savedNotes.filter((note) => note.id !== noteToEdit.id);
+  savedNotes = newArray;
+}
+
+function deleteNote() {
+  const noteToDelete = document.querySelector(".note_selected");
+  noteToDelete.remove();
+  const noteToDeleteTitle = titleInputField.value;
+  updateLS(noteToDeleteTitle);
+  console.log(noteToDeleteTitle);
+  clearInputField();
+}
+
+function renderSavedNotes() {
+  if (getNotesFromLS() !== null) {
+    savedNotes = getNotesFromLS();
+    notesContainer.innerHTML = "";
+    let sortedSavedNotes = savedNotes.reverse();
+    sortedSavedNotes.forEach((note) => getNoteCardTemplate(note));
+    savedNotes.reverse();
+  }
+}
+
 function getNoteCardTemplate(oneNote) {
   const noteCard = document.createElement("div");
   noteCard.classList.add("note_card");
-  noteCard.addEventListener('click', () => {editNote(oneNote, noteCard)});
+  noteCard.addEventListener("click", () => {
+    editNote(oneNote, noteCard);
+  });
   const title = document.createElement("span");
   title.classList.add("note_title");
   title.innerText = oneNote.noteTitle;
@@ -99,43 +132,19 @@ function getNotesFromLS() {
   return savedNotes;
 }
 
-function deleteNote() {
-    const noteToDelete = document.querySelector(".note_selected");
-    noteToDelete.remove();
-    const noteToDeleteTitle = titleInputField.value;
-    updateLS(noteToDeleteTitle);
-    console.log(noteToDeleteTitle);
-    clearInputField();
-}
-
 function updateLS(title) {
-    let updatedArray = savedNotes.filter(note => note.noteTitle !== title);
-    savedNotes = updatedArray;
-    saveNotesInLS();
-}
-
-function editNote(noteToEdit, clickedCard) {
-    document.querySelectorAll('.note_card').forEach(card => card.classList.remove('note_selected'));
-    clickedCard.classList.add("note_selected");
-      if(titleInputField.value === noteToEdit.noteTitle && contentInputField.value === noteToEdit.noteBody) {
-    noteToEdit.lastUpdated = noteToEdit.lastUpdate;
-  } else {
-    titleInputField.value = noteToEdit.noteTitle;
-    contentInputField.value = noteToEdit.noteBody;
-    noteToEdit.lastUpdated = new Date().toLocaleString(); 
-  }
-    console.log('noteToEdit ID', noteToEdit.id);
-    let newArray = savedNotes.filter(note => note.id !== noteToEdit.id);
-    savedNotes = newArray;
+  let updatedArray = savedNotes.filter((note) => note.noteTitle !== title);
+  savedNotes = updatedArray;
+  saveNotesInLS();
 }
 
 function renderToastMessage() {
-alertMessage.classList.remove('hidden');
-setTimeout(() => {
+  alertMessage.classList.remove("hidden");
+  setTimeout(() => {
     dismissAlert();
-}, 3000);
+  }, 3000);
 }
 
 function dismissAlert() {
-    alertMessage.classList.add('hidden');
+  alertMessage.classList.add("hidden");
 }
